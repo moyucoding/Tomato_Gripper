d8c2db3f0c9266aff6d2c57dc4a13d56d89b49cd
moyucoding <tharealzhong@gmail.com>
2021-08-04 15:36:34 +0800

    210804
    Added:
    Gripper Control

--- ArmHandle.py
+++ ArmHandle.py
@@ -24,9 +24,6 @@ class ArmHandler:
     
     def movePos(self, target):
         buf_size = 1024
-        #Target to string
-        #target = 'speed,0,0,0,0,0,0,0'
-        #Send string
         self.arm.send(bytes(target, 'utf-8'))
         print('[Info] Send movePos request to arm.') 
         #Get result
@@ -41,20 +38,31 @@ class ArmHandler:
     def getPos(self):
         #Send msg
         self.arm.send(bytes('0', 'utf-8'))
+        print('[Info] Send getPos request to arm.') 
         #Get result
         result = self.arm.recv(self.buf_size)
         #Result to string
-        pos = '0,0,0,0,0,0,0'
-        return pos
+        msg = str(result, 'utf-8')
+        #Remove \x00
+        msg = msg[:-3]
+        pos = msg.split(',')
+        for i in range(7):
+            pos[i] = round(float(pos[i]), 2)
+        str_pos = ','.join(str(num) for num in pos)
+        return str_pos
 
     def holdGripper(self):
         #Send msg
-        self.arm.send(bytes('4', 'utf-8'))
+        print('[Info] Send holdGripper request to arm.') 
+        self.arm.send(bytes('3', 'utf-8'))
+        time.sleep(0.5)
         pass
 
     def releaseGripper(self):
         #Send msg
-        self.arm.send(bytes('3', 'utf-8'))
+        print('[Info] Send releaseGripper request to arm.')
+        self.arm.send(bytes('4', 'utf-8'))
+        time.sleep(0.5)
         pass
 
     def run(self):
@@ -70,36 +78,42 @@ class ArmHandler:
                     self.movePos(target)
                     #Send result to PLC
                     print('[Info] Get movePos result from arm.')
-                    ret = 'Y.movePos' + ' '*191
+                    ret = 'Y;movePos;' + ' '*190
                     os.write(self.pipe, ret.encode('utf-8'))
                     print('[Info] Send movePos result to PLC')
-                else:
-                    os.write(self.pipe, pipe_raw)
-                '''
-                elif self.request == 'getPos':
+
+                elif self.request[0] == 'getPos':
+                    print('[Info] Get getPos request from PLC.')
                     #Send result to arm
                     pos = self.getPos()
                     #Send result to PLC
-                    self.result = pos
+                    print('[Info] Get getPos result from arm.')
+                    self.result = 'Y;getPos;' + pos + ';' +' '*(190 - len(pos))
                     os.write(self.pipe, self.result.encode('utf-8'))
+                    print('[Info] Send getPos result to PLC')
 
-                elif self.request == 'holdGripper':
+                elif self.request[0] == 'holdGripper':
+                    print('[Info] Get holdGripper request from PLC.')
                     #Send result to gripper
                     self.holdGripper()
                     #Send result to PLC
-                    os.write(self.pipe, 'Y.holdGripper'.encode('utf-8'))
+                    ret = 'Y;holdGripper;' + ' '*186
+                    os.write(self.pipe, ret.encode('utf-8'))
+                    print('[Info] Send holdGripper result to PLC')
 
-                elif self.request == 'releaseGripper':
+                elif self.request[0] == 'releaseGripper':
+                    print('[Info] Get releaseGripper request from PLC.')
                     #Send result to gripper
                     self.releaseGripper()
                     #Send result to PLC
-                    os.write(self.pipe, 'Y.releaseGripper'.encode('utf-8'))
+                    ret = 'Y;releaseGripper;' + ' '*183
+                    os.write(self.pipe, ret.encode('utf-8'))
+                    print('[Info] Send releaseGripper result to PLC')
 
-                elif self.request[0] == 'y' or self.request[0] == 'n':
-                    #Resend to PLC 
-                    self.result = 1
-                '''
+                else:
+                    os.write(self.pipe, pipe_raw)
                 
             except:
                 print('[Error] ArmHandler.')
             time.sleep(self.interval/2)    
+
--- ArmTest.py
+++ ArmTest.py
@@ -24,13 +24,44 @@ def movePos(arm, target, speed):
     while not bool(result.decode('utf-8')):
         result = arm.recv(buf_size)
 
+def getPos(arm):
+    buf_size = 1024
+    arm.send(bytes('0', 'utf-8'))
+    #Get result
+    result = arm.recv(buf_size)
+    print(result)
+    #Result to string
+    pos = '0,0,0,0,0,0,0'
+
+
 def main():
     arm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     arm.connect(('192.168.127.20', 5062))
 
+    '''
     targets = ['219.91,-332.00,289.34,0.21,0.66,-0.65,0.32','636.63,138.27,410.64,0.12,-0.72,0.07,-0.68']
     for target in targets:
         movePos(arm, target, 1)
+    '''
+    getPos(arm)
 
 if __name__ == '__main__':
     main()
+
+    '''
+    b'636.47314453125,138.40185546875,410.58065795898,0.12007275223732,-0.71999657154083,0.069882988929749,-0.67992925643921\x00'
+    '''
+#%%
+a = b'636.47314453125,138.40185546875,410.58065795898,0.12007275223732,-0.71999657154083,0.069882988929749,-0.67992925643921\x00'
+print(type(a))
+str_a = str(a, 'utf-8')
+str_a = str_a[:-3]
+print(str_a)
+array_a = str_a.split(',')
+print(array_a)
+for i in range(7):
+    array_a[i] = round(float(array_a[i]),2)
+print(array_a)
+str_a = ','.join(str(num) for num in array_a)
+print(str_a)
+# %%
